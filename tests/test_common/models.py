f69417697a32a612313e95a51feb3112e75512d1
Nilton Frederico Teixeira <niltonfrederico@pm.me>
2019-02-26 15:45:34 -0300

    Mocked tests for base model

---
+++ tests/test_common/models.py
@@ -0,0 +1,21 @@
+import pytest
+
+
+def test_base_model_creation():
+    pass
+
+
+def test_base_model_update():
+    pass
+
+
+def test_base_model_removal():
+    pass
+
+
+def test_base_model_auto_update_created_at():
+    pass
+
+
+def test_base_model_representation():
+    pass
